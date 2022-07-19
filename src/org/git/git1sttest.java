5466e1b66f607288427f33679b3d7d067783c6c4
unknown <karpagamdot@gmail.com>
2022-07-19 14:19:26 +0530

    tester2 changes file done

--- src/org/git/git1sttest.java
+++ src/org/git/git1sttest.java
@@ -3,9 +3,8 @@ package org.git;
 public class git1sttest {
 	
 	public static void main(String[] args) {
-		System.out.println("hai this test");
-		
-		System.out.println("tester1 file");
+		System.out.println("hai");
+		System.out.println("master location into tester2");
 		System.out.println("changes made");
 	}
 
